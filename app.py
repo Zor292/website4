e3f0dc9f6e370c297c77fd2246ef9a5f3db866fc
8.c2 <firasbouslimi8@gmail.com>
2026-08-18 15:58:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -113,4 +113,5 @@ def roblox_sync():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
